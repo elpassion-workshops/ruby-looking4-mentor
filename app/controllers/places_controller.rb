5b9e2c97dd7466850664f191f78ff827fe73b7aa
Karol Sarnacki <karol.sarnacki@elpassion.com>
2016-09-10 01:10:14 +0200

    Simple Google Places Search

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,8 @@
 class PlacesController < ApplicationController
+  API_KEY = 'AIzaSyAP1vwxyCM--MERNqLnncKfDUsx9YfqvTQ'
+
   def index
-    @places = ['DaftCafe', 'Stixx', 'Casino Olympik']
+    @client = GooglePlaces::Client.new(API_KEY)
+    @places = @client.spots(52.2321841, 20.984737, radius: 200)
   end
 end
